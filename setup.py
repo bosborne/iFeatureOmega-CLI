0af34afcdffdab79600085e08e82c5f0ed63bcda
Brian Osborne <bosborne@alum.mit.edu>
2025-01-06 21:14:55 -0500

    New setup

--- setup.py
+++ setup.py
@@ -11,16 +11,14 @@ setup(
     keywords='iFeatureOmegaCLI',
     description = 'An integrative platform for the prediction/feature engineering, visualization and analysis of features from molecular sequence, structural and ligand data sets',
     license = 'MIT License',
-    url = 'https://github.com/Superzchen/iFeatureOmega-CLI',
+    url = 'https://github.com/bosborne/iFeatureOmega-CLI',
     author = 'SuperZhen',
     author_email = 'chenzhen-win2009@163.com',    
     packages=find_packages("src"),
     package_dir = {'':'src'},
     
     package_data = {
-        # 任何包中含有.txt文件，都包含它
         '': ['*.txt'],
-        # 包含demo包data文件夹中的 *.dat文件
         'iFeatureOmegaCLI': ['data/*.txt', 'data/*.data', 'data_examples/*.csv', 'data_examples/*.txt', 'data_examples/*.pdb', 'parameters/*.json'],
     },
     platforms = 'any',
